41b1203bf91704604b38819bddd79f56869efea6
monmima <monmima@protonmail.com>
2021-01-22 22:14:05 -0500

    reverseAString.js; just added an algo

---
+++ reverseAString.js
@@ -0,0 +1,8 @@
+// Reverse the string that is provided
+
+function reverseAstring(str) {
+    return str.split("").reverse().join("");
+}
+
+console.log(reverseAstring("hello"));
+console.log(reverseAstring("Howdy"));
